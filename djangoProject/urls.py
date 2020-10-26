f2b609b7ac858709ce6d4fb2186abc06d4b220de
Thiago Marcelino <thiagogm1160@gmail.com>
2020-10-26 07:54:53 -0300

    Implementação template.

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 from django.conf.urls import url
 
 urlpatterns = [
+    url('admin/', admin.site.urls),
     url('', TemplateView.as_view(template_name='index.html')),
-    path('admin/', admin.site.urls),
     ]
 
