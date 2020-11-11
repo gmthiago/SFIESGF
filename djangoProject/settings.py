ea3a97bf84080e5525d0e913cfbe1642f6d38d2b
Thiago Marcelino <thiagogm1160@gmail.com>
2020-11-11 17:11:35 -0300

    Ajustes para subir o server.

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '1)fg&_-rxa_3=7sm6wu&2=wqt)^oj^wp01kl2)g8(=!s^*f4ef'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
